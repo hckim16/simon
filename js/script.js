69aa3d40ad3f9be9bf6f813f43028b4e05c4152b
Hyo Chang Kim <hck@hyochangkim.com>
2018-02-15 21:36:55 -0500

    main functionality added, game playable

--- js/script.js
+++ js/script.js
@@ -1,49 +1,96 @@
 
+    userSeq = [];
+    simonSeq = [0, 2, 1];
+    const NUM_OF_LEVELS = 3;
+    var id;
+    var color;
+    var level = 0;
+    var boardSound = [
+        "https://www.soundjay.com/button/button-1.mp3",
+        "https://www.soundjay.com/button/button-2.mp3",
+        "https://www.soundjay.com/button/button-3.mp3",
+        "https://www.soundjay.com/button/button-4.mp3" 
+    ];
+    
     $(function () {
-        userSeq = [];
-        simonSeq = [];
-        var id;
-        var color;
-        var level = 0;
-        var boardSound = [
-            "https://www.soundjay.com/button/button-1.mp3",
-            "https://www.soundjay.com/button/button-2.mp3",
-            "https://www.soundjay.com/button/button-3.mp3",
-            "https://www.soundjay.com/button/button-4.mp3" //blue
-        ];
+       
         $("#start").click(function () {
             level++;
-            startSequence();
+            simonSequence();
         });
-        //simon sequence
-        function startSequence() {
-            $("#timer").text(level);
-            getRandomNum();
-            var i = 0;
-            var myInterval = setInterval(function () {
-                id = simonSeq[i];
-                color = $("#" + id).attr("class").split(" ")[1];
-                addClassSound(id, color);
-                i++;
-                if (i == simonSeq.length) {
-                    clearInterval(myInterval);
-                }
-            }, 1000);
-        }
-        //generate random number
-        function getRandomNum() {
-            var random = Math.floor(Math.random() * 4);
-            simonSeq.push(random);
-        }
-        function addClassSound() {
-            $("#" + id).addClass(color + "-click");
-            playSound();
-            
-            setTimeOut(function () {
-                $("#" + id).removeClass(color + "-click");
-            }, 500);
-        }
-        function playSound(id) {
-        }
+
+        $(".pad").click(function(){
+            id = $(this).attr("id");
+            color = $(this).attr("class").split(" ")[1];
+            userSeq.push(i);
+            addClassSound(id, color);
+            if(!checkUserSeq()){
+                displayErr();
+                userSeq = [];
+            }
+            if(userSeq.length == simonSeq.length && userSeq < NUM_OF_LEVELS){
+                level++;
+                userSeq = [];
+                simonSequence();
+            }
+            if(userSeq.length == NUM_OF_LEVELS){
+                $(".timer").text("Win");
+            }
+        });
+
+        
     });
 
+    function checkUserSeq(){
+        for(var i = 0; i < userSeq.legnth; i++){
+            if(userSeq[i] != simonSeq[i]){
+                return false;
+            }
+        }
+        return true;
+    };
+
+    function displayErr(){
+        var counter = 0;
+        var myError = setInterval(function(){
+            $(".timer").text("Err");
+            counter++;
+            if(counter == 3){
+                $(".timer").text(level);
+                clearInterval(myError);
+                userSeq = [];
+                counter = 0;
+            }
+        }, 500);
+    };
+    //simon sequence
+    function simonSequence() {
+        $("#timer").text(level);
+        //getRandomNum();
+        var i = 0;
+        var myInterval = setInterval(function () {
+            id = simonSeq[i];
+            color = $("#" + id).attr("class").split(" ")[1];
+            addClassSound(id, color);
+            i++;
+            if (i == simonSeq.length) {
+                clearInterval(myInterval);
+            }
+        }, 1000);
+    };
+    //generate random number
+    function getRandomNum() {
+        var random = Math.floor(Math.random() * 4);
+        simonSeq.push(random);
+    };
+    function addClassSound() {
+        $("#" + id).addClass(color + "-click");
+        playSound();
+        setTimeOut(function () {
+            $("#" + id).removeClass(color + "-click");
+        }, 500);
+    };
+    function playSound(id) {
+        var sound = new Audio(boardSound[id]);
+        sound.play();
+    };
---
+++ js/script1.js
@@ -0,0 +1,100 @@
+
+userSeq = [];
+simonSeq = [];
+const NUM_OF_LEVELS = 3;
+var id;
+var color;
+var level = 0;
+var boardSound = [
+    "https://www.soundjay.com/button/button-1.mp3",
+    "https://www.soundjay.com/button/button-5.mp3",
+    "https://www.soundjay.com/button/button-10.mp3",
+    "https://www.soundjay.com/button/button-12.mp3" 
+];
+
+
+$(function(){
+    $("#start").click(function(){
+        level++;
+        simonSequence();
+    })
+
+    $(".pad").click(function(){
+        id = $(this).attr("id");
+        color = $(this).attr("class").split(" ")[1];
+        userSeq.push(id);
+        console.log(id + " " + color);
+        addClassSound(id, color);
+        if(!checkUserSeq()){
+            displayError();
+            userSeq = [];
+        }
+        if(userSeq.length == simonSeq.length && userSeq.length < NUM_OF_LEVELS){
+            level++;
+            userSeq = [];
+            simonSequence();
+        }
+        if(userSeq.length == NUM_OF_LEVELS){
+            $("#timer").text("Win");
+        }
+    })
+})
+
+function checkUserSeq(){
+    for(var i = 0; i < userSeq.length; i++){
+        if(userSeq[i] != simonSeq[i]){
+            return false;
+        }
+    }
+    return true;
+}
+
+function displayError(){
+    console.log("error");
+    var counter = 0;
+    var myError = setInterval(function(){
+        $("#timer").text("Err");
+        counter++;
+        if(counter == 3){
+            $("#timer").text(level);
+            clearInterval(myError);
+            userSeq = [];
+            counter = 0;
+        }
+    }, 500);
+}
+
+function simonSequence(){
+    console.log(level);
+    $("#timer").text(level);
+    getRandomNum();
+    var i = 0;
+    var myInterval = setInterval(function(){
+        id = simonSeq[i];
+        color = $("#" + id).attr("class").split(" ")[1];
+        console.log(id + " " + color);
+        addClassSound(id, color);
+        i++;
+        if(i == simonSeq.length){
+            clearInterval(myInterval);
+        }
+    }, 1000);
+}
+
+function getRandomNum(){
+    var random = Math.floor(Math.random() * 4);
+    simonSeq.push(random);
+}
+
+function addClassSound (id, color){
+    $("#" + id).addClass(color + "-click");
+    playSound(id);
+    setTimeout(function(){
+        $("#" + id).removeClass(color + "-click");
+    }, 500);
+}
+
+function playSound(id){
+    var sound = new Audio(boardSound[id]);
+    sound.play();
+}
